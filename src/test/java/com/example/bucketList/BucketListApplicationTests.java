de93237ff045ded2fb1d5db94eb18cb127e9b6b6
sakurai <mene10morim@gmail.com>
2026-02-24 11:10:51 +0900

    first commit

---
+++ src/test/java/com/example/bucketList/BucketListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bucketList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
