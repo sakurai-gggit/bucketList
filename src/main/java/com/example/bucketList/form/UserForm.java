a4771537c9702eb550f374242172dd06eb24343e
sakurai <mene10morim@gmail.com>
2026-02-25 10:25:18 +0900

    新規登録ページの作成

---
+++ src/main/java/com/example/bucketList/form/UserForm.java
@@ -0,0 +1,15 @@
+package com.example.bucketList.form;
+
+import lombok.Data;
+
+@Data
+
+public class UserForm {
+
+	private String email;
+
+	private String password;
+
+	private String passwordConfirmation;
+
+}
